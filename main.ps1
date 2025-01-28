14a4c90462c4c0b4c124460f8209ebdb0bc1ced6
Joe Dwumfour <6957676+GHArchitecht@users.noreply.github.com>
2025-01-28 13:14:32 -0500

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,1 +1,9 @@
 
+
+# Step 1: Install applications
+Write-Host "Starting application installations..."
+./install_apps.ps1
+
+# Step 2: Apply system settings
+Write-Host "Configuring system settings..."
+./system_setup.ps1
